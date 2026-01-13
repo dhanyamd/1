b0a29417a92a5381741e66b70c8b3ef84f12a9d4
Dhanya MD <dhanya13md@gmail.com>
2026-01-13 19:16:36 +0530

    trpc setup

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import {PrismaClient} from "@/generated/prisma" 
+
+const globalForPrisma = global as unknown as {
+    prisma : PrismaClient;
+};
+
+const prisma = new PrismaClient();
+
+if (process.env.NODE_ENV !== "production"){
+    globalForPrisma.prisma = prisma
+}
+
+export default prisma;
